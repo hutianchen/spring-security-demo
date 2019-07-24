101c2359d8224cfbfc998e87baaf9248f67a0c16
hutianchen <hutianchen@olayc.cn>
2019-07-24 17:03:40 +0800

    redis服务关闭

--- src/main/java/com/spring/security/test/demo/service/impl/ResourceServiceImpl.java
+++ src/main/java/com/spring/security/test/demo/service/impl/ResourceServiceImpl.java
@@ -16,33 +16,35 @@ import java.util.concurrent.TimeUnit;
 
 @Service
 public class ResourceServiceImpl implements ResourceService {
-    @Autowired
-    RedissonClient redissonClient;
-    @Autowired
-    ResourceServiceMapper serviceMapper;
+//    @Autowired
+//    RedissonClient redissonClient;
+//    @Autowired
+//    ResourceServiceMapper serviceMapper;
     static final String REDISKEY = "REDISKEY";
     @Override
     public List<ResourceModel> queryResource(Integer roleCode) {
-        RBucket<List<ResourceModel>> url = redissonClient.getBucket(REDISKEY + roleCode);
-        //缓存中获取信息
-        if (url.isExists()) {
-            return url.get();
-        } else {
-            //TODO 查询数据库
-            List<ResourceModel> roleId = serviceMapper.getRoleId(roleCode);
-            RBucket<List<ResourceModel>> rBucket = redissonClient.getBucket(REDISKEY + roleCode);
-            rBucket.set(roleId, 60 * 60 * 2, TimeUnit.SECONDS);
-            return roleId;
-        }
+//        RBucket<List<ResourceModel>> url = redissonClient.getBucket(REDISKEY + roleCode);
+////        //缓存中获取信息
+////        if (url.isExists()) {
+////            return url.get();
+////        } else {
+////            //TODO 查询数据库
+////            List<ResourceModel> roleId = serviceMapper.getRoleId(roleCode);
+////            RBucket<List<ResourceModel>> rBucket = redissonClient.getBucket(REDISKEY + roleCode);
+////            rBucket.set(roleId, 60 * 60 * 2, TimeUnit.SECONDS);
+////            return roleId;
+////        }
+        return null;
     }
     @Override
     public Integer queryRoleInfoByManger(String manager) {
         //查询role_code
-        Integer roleCode=null;
-        List<ManagerRoleModel> list = serviceMapper.selectRoleCode(manager);
-        if(!CollectionUtils.isEmpty(list)){
-             roleCode = list.get(0).getRoleCode();
-        }
-        return roleCode;
+//        Integer roleCode=null;
+//        List<ManagerRoleModel> list = serviceMapper.selectRoleCode(manager);
+//        if(!CollectionUtils.isEmpty(list)){
+//             roleCode = list.get(0).getRoleCode();
+//        }
+//        return roleCode;
+        return 0;
     }
 }
